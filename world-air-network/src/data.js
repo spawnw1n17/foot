f79eb590858cba230c301de5bc015ffa672fe25e
spawnw1n17 <ppddaa.2017@gmail.com>
2026-07-30 01:24:40 +0300

    Добавить данные городов и улучшений AeroSphere

---
+++ world-air-network/src/data.js
@@ -0,0 +1,97 @@
+export const CITY_CATALOG = [
+  { id: 'moscow', name: 'Москва', country: 'Россия', continent: 'Европа', lat: 55.7558, lon: 37.6173, population: 13149803 },
+  { id: 'spb', name: 'Санкт-Петербург', country: 'Россия', continent: 'Европа', lat: 59.9343, lon: 30.3351, population: 5597763 },
+  { id: 'kazan', name: 'Казань', country: 'Россия', continent: 'Европа', lat: 55.7961, lon: 49.1064, population: 1314685 },
+  { id: 'yekaterinburg', name: 'Екатеринбург', country: 'Россия', continent: 'Азия', lat: 56.8389, lon: 60.6057, population: 1544376 },
+  { id: 'novosibirsk', name: 'Новосибирск', country: 'Россия', continent: 'Азия', lat: 55.0084, lon: 82.9357, population: 1635338 },
+  { id: 'vladivostok', name: 'Владивосток', country: 'Россия', continent: 'Азия', lat: 43.1155, lon: 131.8855, population: 603519 },
+  { id: 'minsk', name: 'Минск', country: 'Беларусь', continent: 'Европа', lat: 53.9006, lon: 27.559, population: 1996553 },
+  { id: 'warsaw', name: 'Варшава', country: 'Польша', continent: 'Европа', lat: 52.2297, lon: 21.0122, population: 1861644 },
+  { id: 'berlin', name: 'Берлин', country: 'Германия', continent: 'Европа', lat: 52.52, lon: 13.405, population: 3878100 },
+  { id: 'paris', name: 'Париж', country: 'Франция', continent: 'Европа', lat: 48.8566, lon: 2.3522, population: 2102650 },
+  { id: 'london', name: 'Лондон', country: 'Великобритания', continent: 'Европа', lat: 51.5072, lon: -0.1276, population: 8866180 },
+  { id: 'madrid', name: 'Мадрид', country: 'Испания', continent: 'Европа', lat: 40.4168, lon: -3.7038, population: 3332035 },
+  { id: 'rome', name: 'Рим', country: 'Италия', continent: 'Европа', lat: 41.9028, lon: 12.4964, population: 2755309 },
+  { id: 'istanbul', name: 'Стамбул', country: 'Турция', continent: 'Азия', lat: 41.0082, lon: 28.9784, population: 15655924 },
+  { id: 'cairo', name: 'Каир', country: 'Египет', continent: 'Африка', lat: 30.0444, lon: 31.2357, population: 10230350 },
+  { id: 'lagos', name: 'Лагос', country: 'Нигерия', continent: 'Африка', lat: 6.5244, lon: 3.3792, population: 15388000 },
+  { id: 'nairobi', name: 'Найроби', country: 'Кения', continent: 'Африка', lat: -1.2921, lon: 36.8219, population: 4397073 },
+  { id: 'cape-town', name: 'Кейптаун', country: 'ЮАР', continent: 'Африка', lat: -33.9249, lon: 18.4241, population: 4770313 },
+  { id: 'dubai', name: 'Дубай', country: 'ОАЭ', continent: 'Азия', lat: 25.2048, lon: 55.2708, population: 3655000 },
+  { id: 'delhi', name: 'Дели', country: 'Индия', continent: 'Азия', lat: 28.6139, lon: 77.209, population: 16787941 },
+  { id: 'mumbai', name: 'Мумбаи', country: 'Индия', continent: 'Азия', lat: 19.076, lon: 72.8777, population: 12478447 },
+  { id: 'bangkok', name: 'Бангкок', country: 'Таиланд', continent: 'Азия', lat: 13.7563, lon: 100.5018, population: 10539000 },
+  { id: 'beijing', name: 'Пекин', country: 'Китай', continent: 'Азия', lat: 39.9042, lon: 116.4074, population: 21858000 },
+  { id: 'shanghai', name: 'Шанхай', country: 'Китай', continent: 'Азия', lat: 31.2304, lon: 121.4737, population: 24874500 },
+  { id: 'seoul', name: 'Сеул', country: 'Республика Корея', continent: 'Азия', lat: 37.5665, lon: 126.978, population: 9386034 },
+  { id: 'tokyo', name: 'Токио', country: 'Япония', continent: 'Азия', lat: 35.6762, lon: 139.6503, population: 14094034 },
+  { id: 'singapore', name: 'Сингапур', country: 'Сингапур', continent: 'Азия', lat: 1.3521, lon: 103.8198, population: 5917600 },
+  { id: 'jakarta', name: 'Джакарта', country: 'Индонезия', continent: 'Азия', lat: -6.2088, lon: 106.8456, population: 10679951 },
+  { id: 'sydney', name: 'Сидней', country: 'Австралия', continent: 'Океания', lat: -33.8688, lon: 151.2093, population: 5312163 },
+  { id: 'auckland', name: 'Окленд', country: 'Новая Зеландия', continent: 'Океания', lat: -36.8509, lon: 174.7645, population: 1695200 },
+  { id: 'new-york', name: 'Нью-Йорк', country: 'США', continent: 'Северная Америка', lat: 40.7128, lon: -74.006, population: 8258035 },
+  { id: 'toronto', name: 'Торонто', country: 'Канада', continent: 'Северная Америка', lat: 43.6532, lon: -79.3832, population: 3025647 },
+  { id: 'chicago', name: 'Чикаго', country: 'США', continent: 'Северная Америка', lat: 41.8781, lon: -87.6298, population: 2664452 },
+  { id: 'los-angeles', name: 'Лос-Анджелес', country: 'США', continent: 'Северная Америка', lat: 34.0522, lon: -118.2437, population: 3820914 },
+  { id: 'mexico-city', name: 'Мехико', country: 'Мексика', continent: 'Северная Америка', lat: 19.4326, lon: -99.1332, population: 9209944 },
+  { id: 'havana', name: 'Гавана', country: 'Куба', continent: 'Северная Америка', lat: 23.1136, lon: -82.3666, population: 2137847 },
+  { id: 'bogota', name: 'Богота', country: 'Колумбия', continent: 'Южная Америка', lat: 4.711, lon: -74.0721, population: 7968095 },
+  { id: 'lima', name: 'Лима', country: 'Перу', continent: 'Южная Америка', lat: -12.0464, lon: -77.0428, population: 10004741 },
+  { id: 'sao-paulo', name: 'Сан-Паулу', country: 'Бразилия', continent: 'Южная Америка', lat: -23.5505, lon: -46.6333, population: 11451999 },
+  { id: 'buenos-aires', name: 'Буэнос-Айрес', country: 'Аргентина', continent: 'Южная Америка', lat: -34.6037, lon: -58.3816, population: 3120612 }
+];
+
+export const PLANE_LEVELS = [
+  { name: 'Региональный', capacity: 28, speed: 560, efficiency: 1.0, upgradeCost: 0 },
+  { name: 'Турбовинтовой', capacity: 48, speed: 630, efficiency: 0.94, upgradeCost: 145000 },
+  { name: 'Узкофюзеляжный', capacity: 82, speed: 760, efficiency: 0.88, upgradeCost: 285000 },
+  { name: 'Магистральный', capacity: 132, speed: 840, efficiency: 0.82, upgradeCost: 520000 },
+  { name: 'Дальнемагистральный', capacity: 210, speed: 905, efficiency: 0.76, upgradeCost: 920000 }
+];
+
+export const AIRPORT_LEVELS = [
+  { name: 'Аэродром', capacity: 110, gates: 1, upgradeCost: 0 },
+  { name: 'Региональный аэропорт', capacity: 240, gates: 2, upgradeCost: 190000 },
+  { name: 'Международный аэропорт', capacity: 460, gates: 3, upgradeCost: 420000 },
+  { name: 'Крупный хаб', capacity: 820, gates: 5, upgradeCost: 790000 },
+  { name: 'Мегахаб', capacity: 1400, gates: 8, upgradeCost: 1450000 }
+];
+
+export const RESEARCH_ITEMS = [
+  {
+    id: 'smart-dispatch',
+    title: 'Умная диспетчеризация',
+    description: 'Самолёты эффективнее забирают транзитных пассажиров.',
+    cost: 90,
+    effect: 'Вместимость рейсов +12%'
+  },
+  {
+    id: 'green-fuel',
+    title: 'Экономичное топливо',
+    description: 'Снижение расхода на всех направлениях.',
+    cost: 120,
+    effect: 'Расходы на топливо −18%'
+  },
+  {
+    id: 'fast-turnaround',
+    title: 'Быстрый разворот',
+    description: 'Сокращённое обслуживание самолётов между рейсами.',
+    cost: 135,
+    effect: 'Скорость рейсов +15%'
+  },
+  {
+    id: 'dynamic-pricing',
+    title: 'Динамические тарифы',
+    description: 'Доход растёт на перегруженных и дальних направлениях.',
+    cost: 155,
+    effect: 'Доход с пассажира +16%'
+  }
+];
+
+export const OBJECTIVES = [
+  { id: 'cities-6', title: 'Региональная сеть', description: 'Открыть 6 аэропортов', reward: 200000 },
+  { id: 'routes-8', title: 'Связующая компания', description: 'Построить 8 маршрутов', reward: 280000 },
+  { id: 'continents-3', title: 'Межконтинентальный рывок', description: 'Работать на 3 континентах', reward: 420000 },
+  { id: 'passengers-5000', title: 'Выбор пассажиров', description: 'Перевезти 5 000 человек', reward: 500000 },
+  { id: 'reputation-92', title: 'Премиальный сервис', description: 'Достичь репутации 92', reward: 350000 }
+];
