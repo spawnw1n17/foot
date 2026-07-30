42ae6fc15e67504318d85f299bdc6a3ba31bf0b9
spawnw1n17 <ppddaa.2017@gmail.com>
2026-07-30 03:07:30 +0300

    Кэшировать стабильный мост команд города

--- world-air-network/sw.js
+++ world-air-network/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aerosphere-v6';
+const CACHE_NAME = 'aerosphere-v7';
 const APP_SHELL = [
   './',
   './index.html',
@@ -10,6 +10,7 @@ const APP_SHELL = [
   './src/engine.js',
   './src/game.js',
   './src/gesture-controls.js',
+  './src/stable-city-clicks.js',
   './src/route-guard.js',
   './src/polish.js'
 ];
